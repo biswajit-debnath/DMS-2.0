a47f2b9ba225489cc8e94684030afb44615f92b9
biswajit-debnath <biswajitdebnath405@gmail.com>
2021-02-26 17:59:47 +0530

    search-added

--- src/components/Reusable/Product Card/Card.jsx
+++ src/components/Reusable/Product Card/Card.jsx
@@ -13,7 +13,9 @@ import img1 from '../../../assets/product1.png';
 
 export default function Card({name,img}) {
     const [heart, setHeart] = useState(false);
-    const showHeart = () => setHeart(!heart);
+    const showHeart = () => {
+        setHeart(!heart);
+    }
 
     return (
         <div className='product-wrapper'>
