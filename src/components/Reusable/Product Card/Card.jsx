81c375d97eebd61db2161ff8c302e43e9b91ff29
biswajit-debnath <biswajitdebnath405@gmail.com>
2021-02-28 17:47:24 +0530

    nothing

--- src/components/Reusable/Product Card/Card.jsx
+++ src/components/Reusable/Product Card/Card.jsx
@@ -8,10 +8,47 @@ import img1 from '../../../assets/product1.png';
 
 export default function Card({name,img}) {
     const [heart, setHeart] = useState(false);
+
+    
     const showHeart = () => {
         localStorage.
         setHeart(!heart);
+    
+
+    // x=[[11, 12, 13, 14, 15,16],
+    //    [21, 22, 23, 24, 25,26],
+    //    [31, 32, 33, 34, 35,36],
+    //    [41, 42, 43, 44, 45,46],
+    //    [51, 52, 53, 54, 55,56],
+    //    [61, 62, 63, 64, 65,66]]
+
+    x=[[-1 ,-1 ,0 ,-9 ,-2 ,-2],
+        [-2 ,-1 ,-6 ,-8 ,-2 ,-5],
+        [-1 ,-1 ,-1 ,-2 ,-3 ,-4],
+        [-1 ,-9 ,-2 ,-4 ,-4 ,-5],
+        [-7 ,-3 ,-3 ,-2 ,-9 ,-9],
+        [-1 ,-3 ,-1 ,-2 ,-4 ,-5]]
         
+        let hGlassM=[[],[],[],[]];
+        let tempSubArray=[[],[],[]];
+        let maxHourGlassSum=Number.NEGATIVE_INFINITY;
+
+        for(let hGlassMIty=0; hGlassMIty<4;hGlassMIty++){
+            for(let outArrayItx=0; outArrayItx<4;outArrayItx++){ 
+                tempSubArray=[[],[],[]];
+                for(let givenArrayy=hGlassMIty, i=0; givenArrayy<hGlassMIty+3;givenArrayy++, i++){
+                    for(let givenArrayx=outArrayItx, j=0; givenArrayx<outArrayItx+3;givenArrayx++, j++){
+                        tempSubArray[i][j]= x[givenArrayy][givenArrayx];   
+                    }
+                }
+                hGlassM[hGlassMIty][outArrayItx]= tempSubArray[0][0]+tempSubArray[0][1]+tempSubArray[0][2]+tempSubArray[1][1]+tempSubArray[2][0]+tempSubArray[2][1]+tempSubArray[2][2];
+                if(hGlassM[hGlassMIty][outArrayItx]>maxHourGlassSum)
+                    maxHourGlassSum=hGlassM[hGlassMIty][outArrayItx];
+            }
+        }
+    
+    
+    
     }
 
     return (
